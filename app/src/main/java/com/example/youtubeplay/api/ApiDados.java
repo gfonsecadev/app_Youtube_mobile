2b94855471c1d979dd69447f67a47222c7861d1c
gfonsecadev <gildrive04@gmail.com>
2024-06-15 17:47:07 -0300

    api key no local.properties

--- app/src/main/java/com/example/youtubeplay/api/ApiDados.java
+++ app/src/main/java/com/example/youtubeplay/api/ApiDados.java
@@ -1,8 +1,10 @@
 package com.example.youtubeplay.api;
 
+import com.example.youtubeplay.BuildConfig;
+
 public class ApiDados {
     public static String URL_BASE = "https://www.googleapis.com/youtube/v3/";
-    public static String API_KEY = "AIzaSyDNO85PRK-bbMb9CV0Kb8_PfcDdWSbB5tk";
+    public static String API_KEY = BuildConfig.API_KEY_YOUTUBE;//AQUI UTILIZE A SUA PRÓPRIA API_KEY.
     public static String CANAL_ID = "UCffDXn7ycAzwL2LDlbyWOTw";
 
 
